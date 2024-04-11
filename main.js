4c0f6117780fc2bd8a23e6aa099220adb3bfde68
Guilherme <guilhermersouza34@gmail.com>
2024-04-11 15:33:58 -0300

    Colocando os Atributos

--- main.js
+++ main.js
@@ -19,5 +19,11 @@ const carroDaMaria = {
 }
 
 function carro(modelo,fabricante,anoModelo,anoFabricacao) {
-    
+    this.modelo = modelo
+    this.fabricante = fabricante
+    this.anoModelo = anoModelo
+    this.anoFabricacao = anoFabricacao
+    this.acelerar = function() {
+        console.log('Acelerando...')
+    }
 }

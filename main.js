f33a37d1974b626b8706c4599252ee9237d12669
Guilherme <guilhermersouza34@gmail.com>
2024-04-11 15:32:38 -0300

    Primeiros Passos Com OOP Com Javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+const carroDoJoao = {
+    modelo: 'Fiesta',
+    fabricante: 'Ford',
+    anoModelo: 2020,
+    anoFabricacao: 2019,
+    acelerar: function() {
+        console.log('Acelerando...')
+    }
+}
+
+const carroDaMaria = {
+    modelo: 'Ka',
+    fabricante: 'Ford',
+    anoModelo: 2021,
+    anoFabricacao: 2020,
+    acelerar: function() {
+        console.log('Acelerando...')
+    }
+}
+
+function carro(modelo,fabricante,anoModelo,anoFabricacao) {
+    
+}

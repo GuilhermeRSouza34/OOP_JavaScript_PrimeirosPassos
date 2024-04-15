0ae692c65471367804850b869aa308f05b673803
Guilherme <guilhermersouza34@gmail.com>
2024-04-15 20:50:50 -0300

    Aula de Herança

--- funcionario.js
+++ funcionario.js
@@ -3,16 +3,18 @@ function Pessoa(nome) {
     this.dizOi = function(){
         console.log(this.nome+"Diz Olá");
     }
+    this.dizCargo = function() {
+        console.log(this.cargo);
+    }
 }
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
     this.salario = salario;
+    let salario = 0;
+
+    Pessoa.call(this, nome)
 }
 
-const pessoa1 = new Pessoa("Maria")
 const Funcionario1 = new Funcionario("Maria", "Dev-Front-End", 5000);
-pessoa1.dizOi();
-
-console.log(pessoa1);
-console.log(Funcionario1);
+Funcionario1.dizOi();
